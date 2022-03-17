649c9c9c6e003a17b5767ec15544dab097318616
cndqhrwogus <cndqhrwogus@naver.com>
2022-03-17 14:44:07 +0900

    dot-line detection

--- center_line_detection.py
+++ center_line_detection.py
@@ -10,28 +10,32 @@ def canny(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     kernel = 5
     blur = cv2.GaussianBlur(gray,(kernel, kernel),0)
-    canny = cv2.Canny(gray, 50, 150)
+    canny = cv2.Canny(blur, 170, 200)
     return canny
 
 def region_of_interest(canny):
     height = canny.shape[0]
     width = canny.shape[1]
     mask = np.zeros_like(canny)
+    rectangle = np.array([[ #test
+    (290, 540),
+    (557, 380),
+    (650, 380),
+    (860, 540),]], np.int32)
     # triangle = np.array([[
-    # (200, height),
-    # (800, 350),
-    # (1200, height),]], np.int32)
-    triangle = np.array([[
-    (50, height),
-    (320, 110),
-    (500, height),]], np.int32)
-    cv2.fillPoly(mask, triangle, 255)
-    masked_image = cv2.bitwise_and(canny, mask)
+    # (290, 540),
+    # (460, 440),
+    # (730, 440)
+    # (860, 540),]], np.int32)
+    masked = cv2.fillPoly(mask, rectangle, 255)
+    cv2.imshow("asddasd",masked)
+    masked_image = cv2.bitwise_and(canny, masked)
     return masked_image
 
 def houghLines(cropped_canny):
-    return cv2.HoughLinesP(cropped_canny, 2, np.pi/180, 100, 
-        np.array([]), minLineLength=40, maxLineGap=5)
+    return cv2.HoughLinesP(cropped_canny, 1, np.pi/180, 30, 
+        np.array([]),minLineLength=10, maxLineGap=200)
+
 
 def addWeighted(frame, line_image):
     return cv2.addWeighted(frame, 0.8, line_image, 1, 1)
@@ -52,10 +56,9 @@ def display_lines(img,lines):
 
 def make_points(image, line):
     slope, intercept = line
-    #print(line)
     try:
-        y1 = int(image.shape[0])
-        y2 = int(y1*3.0/5)      
+        y1 = 540
+        y2 = 380      
         x1 = int((y1 - intercept)/slope)
         x2 = int((y2 - intercept)/slope)
         return [[x1, y1, x2, y2]]
@@ -65,14 +68,14 @@ def make_points(image, line):
 def average_slope_intercept(image, lines):
     left_fit = []
     right_fit = []
-
-    if lines is None:
-        return None
+    print(lines)
     for line in lines:
-        for x1, y1, x2, y2 in line:
-            fit = np.polyfit((x1,x2), (y1,y2), 1)
-            slope = fit[0]
-            intercept = fit[1]
+        x1,y1,x2,y2 = line.reshape(4)
+        fit = np.polyfit((x1,x2), (y1,y2), 1)
+        print(fit)
+        slope = fit[0]
+        intercept = fit[1]
+        if not(np.abs(slope)*180<95):
             if slope < 0: 
                 left_fit.append((slope, intercept))
             else:
@@ -90,17 +93,15 @@ def average_slope_intercept(image, lines):
         return None
 
 def center_line(image, lines):
-    if lines is None:
-        return None
-    x1_1, y1_1, x2_1, y2_1 = lines[0].reshape(4)
-    x1_2, y1_2, x2_2, y2_2 = lines[1].reshape(4)
-    
-    ym1 = y1_2
-    ym2 = y2_2
-    
-    xm1 = int((x1_1 + x1_2)/2)
-    xm2 = int((x2_1 + x2_2)/2)
     try:
+        x1_1, y1_1, x2_1, y2_1 = lines[0].reshape(4)
+        x1_2, y1_2, x2_2, y2_2 = lines[1].reshape(4)
+    
+        ym1 = y1_2
+        ym2 = y2_2
+    
+        xm1 = int((x1_1 + x1_2)/2)
+        xm2 = int((x2_1 + x2_2)/2)
         cv2.line(image,
                  (xm1,ym1),
                  (xm2,ym2),
@@ -108,21 +109,32 @@ def center_line(image, lines):
                  thickness = 10)
     except OverflowError:
         print("don't find line")
+    except AttributeError:
+        print("error")
+    except TypeError:
+        print("error")
     return image
 
-cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc('M','J','P','G'))
+cap = cv2.VideoCapture("test3.mp4")
+#cap.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc('M','J','P','G'))
 while True:
     _, frame = cap.read()
     canny_image = canny(frame)
     cropped_canny = region_of_interest(canny_image)
     lines = houghLines(cropped_canny)
+    #print(lines)
     averaged_lines = average_slope_intercept(frame, lines)
     line_image = display_lines(frame, averaged_lines)
+    line_image1 = display_lines(frame,lines)
     center_image = center_line(line_image, averaged_lines)
     combo_image = addWeighted(frame, line_image)
-    cv2.imshow("result", combo_image)
-    cv2.imshow("1",line_image)
+    #result_image = addWeighted(combo_image,rectangle_center_image)
+    #rectangle_img = rectangle(combo_image,averaged_lines)
+    #cv2.imshow("result", combo_image)
+    cv2.imshow("mask",combo_image)
+    cv2.imshow("img",line_image)
+    cv2.imshow("asd",line_image1)
+    #cv2.imshow("crop",canny_image)
     if cv2.waitKey(1) & 0xFF == ord('a'):
         break
 
