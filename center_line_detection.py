e0524afbc2ef919ea8a764d318caea4173ceaa57
cndqhrwogus <cndqhrwogus@naver.com>
2022-03-20 16:42:40 +0900

    위에서 바라보는 모습 코딩: 더욱 정확한 센터값을 만들 수 있다 위에서 바라본 상태에서 센터값을 원래의 사진에서 그리는게 중요

--- center_line_detection.py
+++ center_line_detection.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import cv2
 import numpy as np
+import argparse
 
 def canny(img):
     if img is None:
@@ -17,20 +18,32 @@ def rectangle_line(img):
     rectangle_img = cv2.rectangle(img,(450,410),(760,480),(255,255,255),3)
     return rectangle_img
 
+def warped_image(image):
+    pts =  np.array([[ #top_left,top_right,bottom_left,bottom_right
+    (545,390),
+    (680,390),
+    (290,540),
+    (850,540),]],dtype="float32")#290,540 850,540 545,390 680.390
+    point = np.array([[0,0],[200,0],[0,300],[200,300]],dtype="float32")
+    M = cv2.getPerspectiveTransform(pts,point)
+    warped_img = cv2.warpPerspective(image,M,(200,300))
+    
+    return warped_img
+
 def region_of_interest(canny):
     height = canny.shape[0]
     width = canny.shape[1]
     mask = np.zeros_like(canny)
-    rectangle = np.array([[ 
-    (290, 540),
-    (557, 390),
-    (650, 390),
-    (860, 540),]], np.int32)
-    # rectangle = np.array([[ #test
-    # (340, 540),
-    # (340, 420),
-    # (850, 420),
-    # (850, 540),]], np.int32)
+    # rectangle = np.array([[ 
+    # (290, 540),
+    # (557, 390),
+    # (650, 390),
+    # (860, 540),]], np.int32)
+    rectangle = np.array([[ #test
+    (420, 480),
+    (580, 360),
+    (640, 360),
+    (860, 480),]], np.int32)
     masked = cv2.fillPoly(mask, rectangle, 255)
     cv2.imshow("asddasd",masked)
     masked_image = cv2.bitwise_and(canny, masked)
@@ -45,12 +58,13 @@ def addWeighted(frame, line_image):
     return cv2.addWeighted(frame, 0.8, line_image, 1, 1)
  
 def display_lines(img,lines):
+    print(lines)
     line_image = np.zeros_like(img)
     if lines is not None:
         for line in lines:
             try:
                 for x1, y1, x2, y2 in line:
-                    cv2.line(line_image, (x1,y1),(x2,y2),(0,0,255),10)
+                    cv2.line(line_image, (x1,y1),(x2,y2),(0,0,255),3)
             except OverflowError:
                 print("error")
             except TypeError:
@@ -61,8 +75,8 @@ def display_lines(img,lines):
 def make_points(image, line):
     slope, intercept = line
     try:
-        y1 = 540
-        y2 = 390
+        y1 = 480
+        y2 = 360
         # y1 = 490
         # y2 = 470      
         x1 = int((y1 - intercept)/slope)
@@ -71,36 +85,37 @@ def make_points(image, line):
     except OverflowError:
         print("don't found line")
  
-def average_slope_intercept(image, lines):
-    left_fit = []
-    right_fit = []
-    for line in lines:
-        x1,y1,x2,y2 = line.reshape(4)
-        fit = np.polyfit((x1,x2), (y1,y2), 1)
-        slope = fit[0]
-        intercept = fit[1]
-        if not(np.abs(slope)*180<95):
-            if slope < 0: 
-                left_fit.append((slope, intercept))
-            else:
-                right_fit.append((slope, intercept))
-    if left_fit:
-        left_fit_average  = np.average(left_fit, axis=0)
-        left_line  = make_points(image, left_fit_average)
-    if right_fit:
-        right_fit_average = np.average(right_fit, axis=0)
-        right_line = make_points(image, right_fit_average)
-    if left_fit and right_fit:
-        averaged_lines = np.array([left_line, right_line])
-        return averaged_lines
-    else:
-        return None
+# def average_slope_intercept(image, lines):
+#     left_fit = []
+#     right_fit = []
+#     for line in lines:
+#         x1,y1,x2,y2 = line.reshape(4)
+#         fit = np.polyfit((x1,x2), (y1,y2), 1)
+#         print(fit)
+#         slope = fit[0]
+#         intercept = fit[1]
+#         if not(np.abs(slope)*180<95):
+#             if slope < 0: 
+#                 left_fit.append((slope, intercept))
+#             else:
+#                 right_fit.append((slope, intercept))
+#     if left_fit:
+#         left_fit_average  = np.average(left_fit, axis=0)
+#         left_line  = make_points(image, left_fit_average)
+#     if right_fit:
+#         right_fit_average = np.average(right_fit, axis=0)
+#         right_line = make_points(image, right_fit_average)
+#     if left_fit and right_fit:
+#         averaged_lines = np.array([left_line, right_line])
+#         return averaged_lines
+#     else:
+#         return None
 
 def center_line(image, lines):
     try:
         x1_1, y1_1, x2_1, y2_1 = lines[0].reshape(4)
         x1_2, y1_2, x2_2, y2_2 = lines[1].reshape(4)
-        print(lines)
+        #print(lines)
         # ym1 = y1_2
         # ym2 = y2_2
         ym1 = 480 
@@ -129,16 +144,15 @@ while True:
     cropped_canny = region_of_interest(canny_image)
     lines = houghLines(cropped_canny)
     #print(lines)
-    averaged_lines = average_slope_intercept(frame, lines)
-    line_image = display_lines(frame, averaged_lines)
+    #averaged_lines = average_slope_intercept(frame, lines)
+    line_image = display_lines(frame, lines)
     rectangle_img = rectangle_line(line_image)
-    center_image = center_line(line_image, averaged_lines)
+    #center_image = center_line(line_image, averaged_lines)
     combo_image = addWeighted(frame, line_image)
-    #result_image = addWeighted(combo_image,rectangle_center_image)
-    #rectangle_img = rectangle(combo_image,averaged_lines)
-    #cv2.imshow("result", combo_image)
+    warped_img = warped_image(frame)
+    cv2.imshow("result", warped_img)
     cv2.imshow("mask",combo_image)
-    cv2.imshow("img",line_image)
+    #cv2.imshow("img",line_image)
     #cv2.imshow("crop",canny_image)
     if cv2.waitKey(1) & 0xFF == ord('a'):
         break
