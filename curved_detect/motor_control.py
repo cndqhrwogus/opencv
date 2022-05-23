dd679d5325c9ed107e8122d28f9c27be05ff7636
leejaehyun <cndqhrwogus@naver.com>
2022-05-23 10:11:35 +0900

    소나센서 발견을 1순위로 만들고 opencv 감지를 2순위를 둔다

--- curved_detect/motor_control.py
+++ curved_detect/motor_control.py
@@ -8,50 +8,57 @@ from std_msgs.msg import String, Float32
 from geometry_msgs.msg import Twist,TransformStamped
 
 
-Pub = rospy.Publisher('/cmd_vel',Twist,queue_size=10)
-def sonar_callback(data):
-    global sonar_warning
-    sonar_warning = data.data
+class motor:
 
-def callback(data):
-    end_time = time.time() + 3
-    if sonar_warning == 'FRONT DETECT' or 'LEFT DETECT' or 'RIGHT DETECT':
-        forwardspeed = 0.0
-        anglespeed = 0.0
-    else:
-        if data.data == 'Straight':
-            forwardspeed = 0.2
-            anglespeed = 0.0
-        elif data.data == 'Right Curve':
-            anglespeed = -0.3
-            forwardspeed = 0.1
-        elif data.data == 'Left Curve':
-            forwardspeed = 0.1
-            anglespeed = 0.3
-        elif data.data == 'no line':
-            forwardspeed = -0.2
-            anglespeed = 0.0
-        elif data.data == 'only left':
-            forwardspeed = 0.1
-            anglespeed = 0.3
-        elif data.data == 'only right':
-            forwardspeed = 0.1
-            anglespeed = -0.3
-    twist = Twist()
-    twist.angular.z = anglespeed
-    twist.linear.x = forwardspeed
-    if data.data == 'no line':
-        while time.time() < end_time:
+    def sonar_callback(self,data):
+        self.sonar_warning = data.data
+
+    def opencv_callback(self,data):
+        self.camera = data.data
+
+    def run(self):
+        rospy.init_node('motor_control')
+        self.sonar_warning = 'safy'
+        self.camera = 'no line'
+        Pub = rospy.Publisher('/cmd_vel',Twist,queue_size=10)
+        Sub = rospy.Subscriber('/lane_detect',String,self.opencv_callback)
+        sonar_sub = rospy.Subscriber('/sonar_range',String,self.sonar_callback)
+        
+        while not rospy.is_shutdown():
+            if ((self.sonar_warning == 'FRONT DETECT') or (self.sonar_warning =='LEFT DETECT') or (self.sonar_warning =='RIGHT DETECT')):
+                while self.sonar_warning != 'safy':
+                    forwardspeed = 0.0
+                    anglespeed = 0.0
+                    twist.angular.z = anglespeed
+                    twist.linear.x = forwardspeed
+                    Pub.publish(twist)
+            else:
+                if (self.camera == 'Straight'):
+                    forwardspeed = 0.2
+                    anglespeed = 0.0
+                elif (self.camera == 'Right Curve'):
+                    anglespeed = -0.3
+                    forwardspeed = 0.1
+                elif (self.camera == 'Left Curve'):
+                    forwardspeed = 0.1
+                    anglespeed = 0.3
+                elif (self.camera == 'no line'):
+                    forwardspeed = 0.0
+                    anglespeed = 0.0
+                elif (self.camera == 'only left'):
+                    forwardspeed = 0.1
+                    anglespeed = 0.3
+                elif (self.camera == 'only right'):
+                    forwardspeed = 0.1
+                    anglespeed = -0.3
+            twist = Twist()
+            twist.angular.z = anglespeed
+            twist.linear.x = forwardspeed
             Pub.publish(twist)
-    else:
-        Pub.publish(twist)
+        rospy.spin()
 
-def run():
-    rospy.init_node('motor_control')
-    Sub = rospy.Subscriber('/lane_detect',String,callback)
-    sonar_sub = rospy.Subscriber('/sonar_range',String,sonar_callback)
-    rospy.spin()
 
 if __name__ == '__main__':
-    run()
+    node = motor()
+    node.run()
 
